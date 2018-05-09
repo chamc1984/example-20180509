ad1f655cf2ac7ab05282c6e8db7dc07a88130b84
chamc1984 <chamc1984@gmail.com>
2018-05-09 17:39:02 +0900

    first commit

---
+++ src/main/java/jp/co/chamc/example/_example/App.java
@@ -0,0 +1,13 @@
+package jp.co.chamc.example._example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
